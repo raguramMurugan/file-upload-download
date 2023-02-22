31a826c53ea5f68511723bf6a25fa13d2a6461bd
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2023-02-22 13:37:11 +0530

    Update FileUploadDownloadController.java

--- FileUploadDownloadController.java
+++ FileUploadDownloadController.java
@@ -1,7 +1,4 @@
 package com.hubino.uploaddownload.fileuploaddownload.controller;
-
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.hubino.uploaddownload.fileuploaddownload.dto.ErrorHandler;
 import com.hubino.uploaddownload.fileuploaddownload.service.FileServiceImpl;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class FileUploadDownloadController {
@@ -20,19 +21,42 @@ public class FileUploadDownloadController {
 	private FileServiceImpl fileService;
 	
 	@PostMapping("/fileUpload")
-	public ResponseEntity<?> fileUpload(@RequestParam("file") MultipartFile files) throws IllegalStateException, IOException
+	public ResponseEntity<?> fileUpload(@Valid @RequestParam("file") MultipartFile files)
 	{
-		 String fileUpload=fileService.fileUpload(files);
-		 return ResponseEntity.status(HttpStatus.OK)
-				 .body(fileUpload);
+		if(files!=null)
+		{
+			fileService.fileUpload(files);
+			return ErrorHandler.response(ErrorHandler.FILE_UPLOAD, HttpStatus.ACCEPTED);
+		}
+		else
+		return ErrorHandler.response(ErrorHandler.FILE_UPLOAD_FAILED,HttpStatus.NOT_FOUND); 
 	}
 	
 	@GetMapping("/downloadFile/{filename}")
-	public ResponseEntity<?> downloadFile(@PathVariable("filename") String filename) throws IOException
+	public ResponseEntity<?> downloadFile(@Valid @PathVariable("filename") String filename)
 	{
-		String downloadedFile=fileService.downloadFile(filename);
-		return ResponseEntity.status(HttpStatus.OK)
-				.body(downloadedFile);
+		if(filename!=null)
+		{
+			String downloadedFile=fileService.downloadFile(filename);
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(downloadedFile);
+		}
+		else
+		return ErrorHandler.response(ErrorHandler.INVALID_FILE_UPLOAD, HttpStatus.UNAUTHORIZED);
 	}
-
+	@GetMapping("/getFileById/{id}")
+	public ResponseEntity<?> getFilesById(@Valid @PathVariable("id") Integer id)
+	{
+		if(id!=null)
+		{
+			ResponseEntity<?> files=fileService.getFilesById(id);
+			return ResponseEntity.status(HttpStatus.OK).body(files);
+		}
+		else 
+		{
+			ErrorHandler.response(ErrorHandler.FILE_ID_EMPTY, HttpStatus.NOT_FOUND);
+		}
+		return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND,HttpStatus.NOT_FOUND);
+		}
 }
+
