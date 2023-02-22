1841471006828280d385d1754ee5fd14242d6eb9
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2023-02-22 13:36:45 +0530

    Updated the Service Impl class
    Taken care of Own Exception
    Added one API for getting files through Id

--- FileServiceImpl.java
+++ FileServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.Base64;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.hubino.uploaddownload.fileuploaddownload.dto.ErrorHandler;
+import com.hubino.uploaddownload.fileuploaddownload.dto.ErrorHandlerResponse;
 import com.hubino.uploaddownload.fileuploaddownload.entity.FileAttachment;
 import com.hubino.uploaddownload.fileuploaddownload.repository.FileAttachmentRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class FileServiceImpl implements FileService{
@@ -23,11 +29,11 @@ public class FileServiceImpl implements FileService{
 	@Autowired
 	private FileAttachmentRepository attachmentRepository;
 	
-	private static final String folderPath="E:\\My PDF_Files/";
+	private static final String folderPath="E:\\My PDF_Files\\";
 	
 	//uploading the File to local Directory
 	
-	public String fileUpload(MultipartFile files) throws IllegalStateException, IOException
+	public ResponseEntity<?> fileUpload(@Valid MultipartFile files) 
 	{
 		String filePath=folderPath+files.getOriginalFilename();
 		
@@ -38,18 +44,22 @@ public class FileServiceImpl implements FileService{
 				.filePath(filePath).build());
 		
 		//copying the files from db to the specified local path
-			files.transferTo(new File(filePath));
-		
+				
+		try {
+				files.transferTo(new File(filePath));
+		} catch (IllegalStateException | IOException e) {
+			return ErrorHandler.response(ErrorHandler.FILE_COPY_FAILED, HttpStatus.NOT_FOUND);
+		}
 		if(fileAttachment!=null)
 		{
-			return "File Uploaded Succesfully:"+filePath;
+			return ErrorHandler.response(ErrorHandler.FILE_UPLOAD, HttpStatus.ACCEPTED);
 		}
-		
-		return "File Uploaded Failed";
+		else
+		return ErrorHandler.response(ErrorHandler.FILE_UPLOAD_FAILED, HttpStatus.BAD_REQUEST);
 	}
 
 	
-	public String downloadFile(String filename) throws IOException
+	public String downloadFile(@Valid String filename)
 	{
 		
 		//getting the filename from the repository as an optional
@@ -59,11 +69,31 @@ public class FileServiceImpl implements FileService{
 			String filePath=attachment.get().getFilePath();
 			
 			//converting it to byte Array using the filepath
-			byte[] downloadedFile=Files.readAllBytes(new File(filePath).toPath());
-			
-			//converting the ByteArray to Base64 String format
-			String base64File=Base64.getEncoder().encodeToString(downloadedFile);
+			byte[] downloadedFile=null;
+			String base64File=null;
+			try {
+				downloadedFile = Files.readAllBytes(new File(filePath).toPath());
+				//converting the ByteArray to Base64 String format
+				base64File=Base64.getEncoder().encodeToString(downloadedFile);
+				return base64File;
+			} catch (IOException e) {
+				ErrorHandler.response(ErrorHandler.BYTE_CONVERSION_FAILED, HttpStatus.BAD_REQUEST);
+			}
+			return base64File;
+
+	}
+
+
+	public ResponseEntity<?> getFilesById(Integer id) {
+		Optional<FileAttachment> attachment=attachmentRepository.findById(id);
 		
-		return base64File;
+		if(attachment.isPresent())
+		{
+			attachment.get();
+			return ErrorHandler.response(ErrorHandler.FILE_RETRIEVED,HttpStatus.OK);
+		}
+		else {
+			return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
 	}
 }
