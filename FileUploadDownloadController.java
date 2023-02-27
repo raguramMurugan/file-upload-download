22122ccd8533b275b139e46dc6d529d26d903c0e
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2023-02-27 20:08:06 +0530

    Updated

--- FileUploadDownloadController.java
+++ FileUploadDownloadController.java
@@ -1,19 +1,17 @@
 package com.hubino.uploaddownload.fileuploaddownload.controller;
 import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.function.ServerRequest.Headers;
-
 import com.hubino.uploaddownload.fileuploaddownload.dto.ErrorHandler;
 import com.hubino.uploaddownload.fileuploaddownload.entity.FileAttachment;
 import com.hubino.uploaddownload.fileuploaddownload.repository.FileAttachmentRepository;
@@ -22,18 +20,16 @@ import com.hubino.uploaddownload.fileuploaddownload.service.FileServiceImpl;
 import jakarta.validation.Valid;
 
 @RestController
+@ControllerAdvice
 public class FileUploadDownloadController {
 	
-	private final long FILE_SIZE=500000;
-	
+	private static final Logger log=LoggerFactory.getLogger(FileServiceImpl.class);
+		
 	@Autowired
 	private FileServiceImpl fileService;
 	
 	@Autowired
 	private FileAttachmentRepository repo;
-	
-	private static final Logger log=LoggerFactory.getLogger(FileUploadDownloadController.class);
-	
 	/**
 	 * 
 	 * @param files
@@ -42,63 +38,69 @@ public class FileUploadDownloadController {
 	@PostMapping("/fileUpload")
 	public ResponseEntity<?> fileUpload(@Valid @RequestParam("file") MultipartFile files)
 	{
+		System.out.println(files.getContentType());
+		String extension=StringUtils.getFilenameExtension(files.getOriginalFilename());
+		System.out.println("extension:"+" "+extension);
 		if(!files.isEmpty() && files!=null)
 		{
-			if(files.getSize() < FILE_SIZE)
+			 if(files.getContentType()==extension)
 			{
-				log.info("file Size:"+" "+files.getSize());
-			if(files.getOriginalFilename().endsWith(".pdf"))
+				 log.info(extension, "Checking the extension");
+				 System.out.println(files.getContentType()!=extension);
+				 return ErrorHandler.response(ErrorHandler.INVALID_FILE_UPLOAD, HttpStatus.NOT_ACCEPTABLE);
+			}else
 			{
-			fileService.fileUpload(files);
-			return ErrorHandler.successResponse(ErrorHandler.FILE_UPLOAD, HttpStatus.ACCEPTED);
-		}
-			else {
-				return ErrorHandler.response(ErrorHandler.INVALID_FILE_UPLOAD, HttpStatus.NOT_ACCEPTABLE);
+			return fileService.fileUpload(files);
 			}
 		}
-			else {
-				return ErrorHandler.response(ErrorHandler.FILE_SIZE_EXCEEDS, HttpStatus.NOT_ACCEPTABLE);
-			}
-		}
+		else
 		return ErrorHandler.response(ErrorHandler.FILE_UPLOAD_FAILED,HttpStatus.NOT_FOUND); 
 	}
-	
 	/**
 	 * 
 	 * @param filename
 	 * @return
 	 */
-	@GetMapping("/downloadFile/{filename}")
-	public ResponseEntity<?> downloadFile(@Valid @PathVariable("filename") String filename)
+	@GetMapping("/downloadFile/")
+	public ResponseEntity<?> downloadFile(@Valid @RequestParam("filename") String filename)
 	{
+		String extension=StringUtils.getFilenameExtension(filename);
+		System.out.println(extension);
 		if(!filename.isEmpty() && filename!=null)
 		{
-		if(filename.endsWith(".pdf"))
+		if(filename.contains(extension))
 		{
-			ResponseEntity<?> downloadedFile=fileService.downloadFile(filename);
-			return ResponseEntity.status(HttpStatus.OK)
-					.body(downloadedFile);
+			return fileService.downloadFile(filename);
 		}
+		else
+		{
+			return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND, HttpStatus.NOT_FOUND);
 		}
-		return ErrorHandler.response(ErrorHandler.INVALID_FILE_UPLOAD, HttpStatus.UNAUTHORIZED);
 	}
+		return ErrorHandler.response(ErrorHandler.FILE_NOT_ATTACHED, HttpStatus.UNAUTHORIZED);
+}
+	
 		/**
 		 * 
 		 * @param id
 		 * @return
 		 */
-	@GetMapping("/getFileById/{id}")
-	public ResponseEntity<?> getFilesById(@Valid @PathVariable("id") Integer id)
+	@GetMapping("/getFileById/")
+	public ResponseEntity<?> getFilesById(@Valid @RequestParam("id") Integer id)
 	{
 		Optional<FileAttachment> fileId=repo.findById(id);
 		if(fileId.isPresent())
 		{
-			ResponseEntity<?> files=fileService.getFilesById(id);
-			log.info("file id is:"+" "+files.equals(fileService.getFilesById(id)));	
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(files);
+		if(id!=null)
+		{
+			log.info("id id null"+" "+id);
+			return fileService.getFilesById(id);
 		}
 		else
-		return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND,HttpStatus.NOT_FOUND);
+			return ErrorHandler.response(ErrorHandler.FILE_ID_EMPTY, HttpStatus.BAD_REQUEST);
 		}
+		return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND,HttpStatus.NOT_FOUND);
+	}
+	
 }
 
