dc77c455bae1cffee9e553e181afd5eec720b045
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2023-02-27 20:08:32 +0530

    Updated

--- FileServiceImpl.java
+++ FileServiceImpl.java
@@ -1,33 +1,24 @@
 package com.hubino.uploaddownload.fileuploaddownload.service;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Base64;
 import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.hubino.uploaddownload.fileuploaddownload.controller.FileUploadDownloadController;
 import com.hubino.uploaddownload.fileuploaddownload.dto.ErrorHandler;
-import com.hubino.uploaddownload.fileuploaddownload.dto.ErrorHandlerResponse;
 import com.hubino.uploaddownload.fileuploaddownload.entity.FileAttachment;
 import com.hubino.uploaddownload.fileuploaddownload.repository.FileAttachmentRepository;
-
 import jakarta.validation.Valid;
 
 @Service
+@Transactional
 public class FileServiceImpl implements FileService{
 	
 	private static final Logger log=LoggerFactory.getLogger(FileServiceImpl.class);
@@ -35,34 +26,39 @@ public class FileServiceImpl implements FileService{
 	@Autowired
 	private FileAttachmentRepository attachmentRepository;
 	
-	private static final String folderPath="E:\\My PDF_Files\\";
+	private static final String folderPath="F:\\My PDF_Files\\";
 	
 	//uploading the File to local Directory
 	
 	public ResponseEntity<?> fileUpload(@Valid MultipartFile files) 
 	{
 		String filePath=folderPath+files.getOriginalFilename();
-		
-		FileAttachment fileAttachment=attachmentRepository.save(
-				FileAttachment.builder()
-				.filename(files.getOriginalFilename())
-				.fileType(files.getContentType())
-				.filePath(filePath).build());
-		
-		//copying the files from db to the specified local path
-				
 		try {
-				files.transferTo(new File(filePath));
+			files.transferTo(new File(filePath));
+			log.info("file Saving..");
+			FileAttachment fileAttachment=attachmentRepository.save(
+					FileAttachment.builder()
+					.filename(files.getOriginalFilename())
+					.fileType(files.getContentType())
+					.filePath(filePath).build());
+			
+			if(fileAttachment!=null)
+			{
+				log.info("File not null");
+				return ErrorHandler.successResponse(ErrorHandler.FILE_UPLOAD,HttpStatus.ACCEPTED);
+			}
+			else
+			log.info("file is null");
+			return ErrorHandler.response(ErrorHandler.FILE_UPLOAD_FAILED, HttpStatus.BAD_REQUEST);
+		
+			
 		} catch (IllegalStateException | IOException e) {
+			log.info("Path not found..."+" "+ e);
 			return ErrorHandler.response(ErrorHandler.FILE_COPY_FAILED, HttpStatus.NOT_FOUND);
 		}
-		if(fileAttachment!=null)
-		{
-			return new ResponseEntity<>(ErrorHandler.FILE_UPLOAD,HttpStatus.ACCEPTED);
-		}
-		else
-		return ErrorHandler.response(ErrorHandler.FILE_UPLOAD_FAILED, HttpStatus.BAD_REQUEST);
-	}
+}
+		
+		
 
 	
 	public ResponseEntity<?> downloadFile(@Valid String filename)
@@ -71,46 +67,45 @@ public class FileServiceImpl implements FileService{
 		//getting the filename from the repository as an optional
 	Optional<FileAttachment> attachment=attachmentRepository.findByFilename(filename);
 	
-			//getting the filepath
-			String filePath=attachment.get().getFilePath();
+			log.info("File status:"+" "+attachment);
 			
-			//converting it to byte Array using the filepath
+			String filePath=null;
 			byte[] downloadedFile=null;
 			String base64File=null;
+			
+			if(!attachment.isPresent())
+			{
+			return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND, HttpStatus.NOT_FOUND);
+			}
+			filePath= attachment.get().getFilePath();
+			
 			try {
 				downloadedFile = Files.readAllBytes(new File(filePath).toPath());
 				//converting the ByteArray to Base64 String format
 				base64File=Base64.getEncoder().encodeToString(downloadedFile);
-			} catch (IOException e) {
-				ErrorHandler.response(ErrorHandler.BYTE_CONVERSION_FAILED, HttpStatus.BAD_REQUEST);
+			} 
+			catch (IOException e) {
+				return ErrorHandler.response(ErrorHandler.BYTE_CONVERSION_FAILED, HttpStatus.BAD_REQUEST);
 			}
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(base64File);
-
+			return ErrorHandler.successResponse(base64File, HttpStatus.ACCEPTED);
 	}
-
 	public ResponseEntity<?> getFilesById(Integer id) {
-		FileAttachment attachment=attachmentRepository.findById(id).get();
-		
-		
+			FileAttachment attachment=attachmentRepository.findById(id).get();
 			byte[] attachmentFile;
+			String base64;
 			
-			if(attachment.getId().equals(id))
+			if(!attachment.getId().equals(id))
 			{
 				log.info("id is:"+attachment.getId().equals(id));
-			try {
-				attachmentFile=Files.readAllBytes(new File(attachment.getFilePath()).toPath());
-				String downloadedFile=Base64.getEncoder().encodeToString(attachmentFile);
-				return ResponseEntity.status(HttpStatus.ACCEPTED).body(downloadedFile);
-			} catch (IOException e) {
-			
-				e.printStackTrace();
-			}
-			
-			}
-			else {
 				return ErrorHandler.response(ErrorHandler.FILE_NOT_FOUND, HttpStatus.NOT_FOUND);
 			}
-		
-		return null;
+				try {
+					attachmentFile=Files.readAllBytes(new File(attachment.getFilePath()).toPath());
+					log.info("File reading..");
+				} catch (IOException e) {
+					return ErrorHandler.response(ErrorHandler.BYTE_CONVERSION_FAILED,HttpStatus.BAD_REQUEST);
+				}	
+				base64=Base64.getEncoder().encodeToString(attachmentFile);
+			return ErrorHandler.successResponse(base64, HttpStatus.ACCEPTED);
 	}
 }
